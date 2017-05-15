cc46d2f2ee21f38054edd63be83a4324e449d01b
Jin Xu <ext.jin.xu@ef.com>
2017-05-15 18:05:39 +0800

    study till service part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from './hero.service';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+  constructor(private heroService: HeroService) { }
+  title = 'Tour of Heros';
+  onSelect = (hero: Hero) => (this.currentHero = hero);
+  heroes: Hero[];
+  currentHero: Hero;
+  getHeroes(): void {
+    this.heroService.getHeroesSlowly().then((data) => {
+      this.heroes = data
+    });
+  }
+  ngOnInit() {
+    this.getHeroes()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; // prevent error for form's two way bind
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { heroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    heroDetailComponent
   ],
   imports: [
     BrowserModule,
